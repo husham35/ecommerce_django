91cdd41f1394f6f5c993bd1efdc091959e52c97b
husham35 <husham35@gmail.com>
2024-10-23 08:22:12 +0000

    additional works on productst and category

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from .views import product_list, product_list_admin, add_product, edit_product, delete_product, product_details
+from .views import product_list, product_list_admin, add_product, edit_product, delete_product, product_details, category_list, add_category, category_details, edit_category, delete_category
 
 urlpatterns = [
+	# product
     path('', product_list, name='product_list'),  # Public product list
-	path('products-list', product_list_admin, name='products-list-admin'),
+	path('products-list', product_list_admin, name='products-list-admin'), # Admin only
     path('add/', add_product, name='add_product'),  # Admin only
 	path('<int:product_id>/', product_details, name='product_details'),
     path('<int:product_id>/edit/', edit_product, name='edit_product'),  # Admin only
     path('<int:product_id>/delete/', delete_product, name='delete_product'),  # Admin only
+	# product category
+    path('category-list', category_list, name='category-list'), # Admin only
+    path('add-category/', add_category, name='add_category'),  # Admin only
+	path('<int:category_id>/', category_details, name='category_details'),
+    path('<int:category_id>/edit/', edit_category, name='edit_category'),  # Admin only
+    path('<int:category_id>/delete/', delete_category, name='delete_category'),  # Admin only
 ]
--- products/views.py
+++ products/views.py
@@ -2,6 +2,8 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from .models import Product
 from .forms import ProductForm
+from .models import Category
+from .forms import CategoryForm
 
 # View for listing products (accessible to all users)
 def product_list(request):
@@ -64,3 +66,61 @@ def delete_product(request, product_id):
         product.delete()
         return redirect('product_list')
     return render(request, 'products/product_confirm_delete.html', {'product': product})
+
+
+# Product category
+# View for listing products (accessible to all users)
+@staff_member_required
+def category_list(request):
+    products = Product.objects.all()
+    return render(request, 'products/category_list.html', {'products': products})
+    # return HttpResponse('Hello')
+
+# Only admin users can add a product
+@staff_member_required
+def add_category(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('category_list')
+    else:
+        form = ProductForm()
+    return render(request, 'products/category_add.html', {'form': form})
+
+
+# Only admin users can update a product
+# @staff_member_required
+def category_details(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    # if request.method == 'POST':
+    #     form = ProductForm(request.POST, request.FILES, instance=product)
+    #     if form.is_valid():
+    #         form.save()
+    #         return redirect('product_list')
+    # else:
+    #     form = ProductForm(instance=product)
+    return render(request, 'products/category_details.html', {'product': product}) # {'product': product})
+
+
+# Only admin users can update a product
+@staff_member_required
+def edit_category(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('category_list')
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'products/category_form.html', {'form': form})
+
+# Only admin users can delete a product
+@staff_member_required
+def delete_category(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    if request.method == 'POST':
+        product.delete()
+        return redirect('category_list')
+    return render(request, 'products/category_confirm_delete.html', {'product': product})
