a5122192373bbcc3d83916993943b581e766fcad
husham35 <husham35@gmail.com>
2024-10-30 08:00:13 +0000

    completed cart module

--- cart/views.py
+++ cart/views.py
@@ -1,23 +1,29 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from products.models import Product  # Replace with your app's product model
 from .cart import Cart
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def add_to_cart(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     cart.add(product=product)
     return redirect('cart_detail')
 
+@login_required
 def remove_from_cart(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
     return redirect('cart_detail')
 
+@login_required
 def cart_detail(request):
     cart = Cart(request)
     return render(request, 'cart/cart_detail.html', {'cart': cart})
 
+@login_required
 def update_cart(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
